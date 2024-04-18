e188590c5b544b433e8a24507166ee5b61aa67a4
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-18 20:53:28 +0530

    Checkpoint 1 done docker database connection working good

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,25 +1,23 @@
 // // //connecting todb here
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// // const prisma = new PrismaClient({
-// //     datasources: {
-// //         db: {
-// //             url: process.env.DATABASE_URL,
-// //         }
-// //     }
-// // });
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        }
+    }
+});
 
-// const prisma = new PrismaClient();
+//making async function to conect to db
+const connectDB = async () => {
+    try {
+        const connectionInstance = await prisma.$connect();
+        console.log(`\n Postgresql connected! DB Host: ${connectionInstance}`);
+    } catch (error) {
+        console.log('Postgresql connection failed:', error);
+        process.exit(1);
+    }
+}
 
-// //making async function to conect to db
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await prisma.$connect();
-//         console.log(`\n Postgresql connected! DB Host: ${connectionInstance}`);
-//     } catch (error) {
-//         console.log('Postgresql connection failed:', error);
-//         process.exit(1);
-//     }
-// }
-
-// export default connectDB;
+export default connectDB;
