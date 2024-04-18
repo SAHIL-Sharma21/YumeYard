0736f560b82130938d699d6f56fdd17d6c68bff2
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-18 17:22:28 +0530

    prisma file

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,23 +1,25 @@
-//connecting todb here
-import { PrismaClient } from '@prisma/client'
+// // //connecting todb here
+// import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        }
-    }
-});
+// // const prisma = new PrismaClient({
+// //     datasources: {
+// //         db: {
+// //             url: process.env.DATABASE_URL,
+// //         }
+// //     }
+// // });
 
-//making async function to conect to db
-const connectDB = async () => {
-    try {
-        const connectionInstance = await prisma.$connect();
-        console.log(`\n Postgresql connected! DB Host: ${connectionInstance}`);
-    } catch (error) {
-        console.log('Postgresql connection failed:', error);
-        process.exit(1);
-    }
-}
+// const prisma = new PrismaClient();
 
-export default connectDB;
+// //making async function to conect to db
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await prisma.$connect();
+//         console.log(`\n Postgresql connected! DB Host: ${connectionInstance}`);
+//     } catch (error) {
+//         console.log('Postgresql connection failed:', error);
+//         process.exit(1);
+//     }
+// }
+
+// export default connectDB;
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,5 @@
 //main entry point for the app
-import connectDB from './db/index.js'
+// import connectDB from './db/index.js'
 import { app } from './app.js'
 import dotenv from 'dotenv'
 
@@ -8,16 +8,20 @@ dotenv.config({
 });
 
 
-connectDB()
-    .then(() => {
-        app.on("error", (error) => {
-            console.log(`Server is facing some error: ${error}`);
-        });
+// connectDB()
+//     .then(() => {
+//         app.on("error", (error) => {
+//             console.log(`Server is facing some error: ${error}`);
+//         });
 
-        app.listen(process.env.PORT || 4000, () => {
-            console.log(`App is listening on PORT: ${process.env.PORT}`);
-        })
-    })
-    .catch((error) => {
-        console.log(`Postgresql connection failed! ${error}`);
-    });
+//         app.listen(process.env.PORT || 4000, () => {
+//             console.log(`App is listening on PORT: ${process.env.PORT}`);
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(`Postgresql connection failed! ${error}`);
+//     });
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`App is listening on PORT: ${process.env.PORT}`);
+})
