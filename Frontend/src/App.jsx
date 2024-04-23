35b5e8879e38ab7898d0c60637ddf22fdd3e9bb0
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-23 15:46:06 +0530

    frontend setup with shadcn ui

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>hello this is front end apllication</h1>
+      <Button variant="destructive"> hello shadcn ui </Button>
+    </>
+  )
+}
+
+export default App
