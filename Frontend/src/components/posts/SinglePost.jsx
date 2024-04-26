483e3eb7c340f34f9fba736dfb3cc4d6d036a85a
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-04-26 17:02:31 +0530

    checkpoint 1

--- Frontend/src/components/posts/SinglePost.jsx
+++ Frontend/src/components/posts/SinglePost.jsx
@@ -1,5 +1,3 @@
-//single post by its id
-
 import { useNavigate, useParams } from "react-router-dom"
 import Navbar from "../Navbar";
 import { useEffect, useState } from "react";
@@ -7,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import {ChevronLeft} from 'lucide-react'
 import axios from "axios";
 import useAuth from "@/utlis/useAuth";
+import PostComments from "./Comment";
 
 const SinglePost = () => {
 
@@ -39,7 +38,7 @@ const SinglePost = () => {
 
   return (
     <>
-        <div className="h-screen  bg-gradient-to-r from-gray-800 to-gray-900">
+        <div className="bg-gradient-to-r from-gray-800 to-gray-900">
             <Navbar />
             <div className="md:w-[80%] mx-auto my-10">
                 <Button onClick={() => navigate("/home")} className="flex justify-start" variant="outline" size="icon">
@@ -47,12 +46,23 @@ const SinglePost = () => {
                 </Button>
             </div>
 
-            <div className="flex flex-row justify-between md:w-[80%] mx-auto items-center">
-                <div className="w-[60%] h-[60%] mx-auto bg-white rounded-md">
-                    <h1 className="text-black text-4xl font-semibold"> single post {post.title}</h1>
+            <div className="flex flex-row justify-between md:w-[85%] mx-auto items-center">
+                <div className="w-[75%]  mx-auto mb-4 bg-white rounded-md">
+                    <h1 className="text-black text-4xl mt-2 text-center p-4 font-semibold">{post.title}</h1>
                     {/* todo make tilte and post visible and make one component to add comment  */}
+                    <div className="sm:p-4 sm:w-full h-[40%] ">
+                         {post && post.postImage ? <img src={post.postImage} className="w-full  max-h-[500px]" /> : <h2>No Post image</h2>}
+                    </div>
+                    <div className="p-2 mb-3">
+                        {post && post.description ? <p className="text-xl font-medium">{post.description}</p> : <p>No description</p>}
+                    </div>
                 </div>
             </div>
+            {/* comment section  */}
+            <div className="md:w-[80%] mx-auto mt-10 pb-5">
+                <PostComments postId={post.postId} />
+            </div>
+            
             <div></div>
         </div>
     </>
